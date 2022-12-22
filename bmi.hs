5177a89abab40c3499efe0e373fbf3f8a2cb1d7f
kashish567 <kashishbhanushali7@gmail.com>
2022-12-22 10:46:36 +0530

    first commit

--- bmi.hs
+++ bmi.hs
@@ -1,6 +1,1 @@
-bmiTell::(RealFloat a)=> a->String
-bmiTell bmi
-  |bmi <=18.5 = "you are underweight , you emo , you!"  
-  |bmi <=25.0 = "you supposely normal.pifft , I bet you are ugly!"
-  |bmi <=30.0 = "you are fat lose some weight , fatty!"
-  |otherwise = "you are a whale congratulation!"
+hi
